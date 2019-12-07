d6caed1054384c21c4e8f7a9b402b7556277d637
misterlucky@tut.by <misterlucky@tut.by>
2019-12-07 21:34:09 +0300

    initial commit

---
+++ src/main/java/by/irun/Time2Run.java
@@ -0,0 +1,13 @@
+package by.irun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Time2Run {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Time2Run.class);
+	}
+
+}
---
+++ src/test/java/by/irun/Time2RunTest.java
@@ -0,0 +1,13 @@
+package by.irun;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class Time2RunTest {
+	
+	@Test
+	public void springApplicationTest(){
+		assertTrue(true);
+	}
+
+}
