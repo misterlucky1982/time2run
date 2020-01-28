48e662dee613270784b817928a2a6b30cb2261fb
misterlucky <misterlucky@tut.by>
2020-01-28 22:02:35 +0300

    front: raceresults

--- src/main/java/by/irun/controller/ApplicationController.java
+++ src/main/java/by/irun/controller/ApplicationController.java
@@ -1,17 +1,30 @@
 package by.irun.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import by.irun.persistance.daoimpl.DataProvider;
 
 @RequestMapping("/app")
 @Controller
 public class ApplicationController {
 	
+	/*
+	 * temporary added for front-end dev
+	 */
+	@Autowired
+	private DataProvider data;
+	
 	@GetMapping("/test")
-	public String getOrderList(){
-		return "raceresult";
+	public ModelAndView getResultListForRace(){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("results", data.getRaceResult(0));
+	    mav.setViewName("raceresult");
+	    return mav;
 	}
 
 }
