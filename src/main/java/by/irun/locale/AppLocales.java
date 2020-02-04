ece2d8bd1b2ed0dae27cf57e1b7763918807a68c
misterlucky <misterlucky@tut.by>
2020-02-04 23:47:45 +0300

    dev: application locales

---
+++ src/main/java/by/irun/locale/AppLocales.java
@@ -0,0 +1,17 @@
+package by.irun.locale;
+
+import java.util.Locale;
+/**
+ * this class provides Locale constants that are used in application
+ * @author A.Dubovik
+ *
+ */
+public class AppLocales {
+	
+	public static final Locale BY = new Locale("by","BY");
+	public static final Locale RU = new Locale("ru","RU");
+	public static final Locale EN = new Locale("en","GB");
+	
+	private AppLocales(){}
+
+}
