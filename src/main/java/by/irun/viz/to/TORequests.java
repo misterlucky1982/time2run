55ded19a4e3ce30ae72269f713398a2fc4d80ddd
misterlucky <misterlucky@tut.by>
2020-02-09 14:29:49 +0300

    dev: to request for runnerResultInfoTO list

--- src/main/java/by/irun/viz/to/TORequests.java
+++ src/main/java/by/irun/viz/to/TORequests.java
@@ -20,6 +20,11 @@ public class TORequests {
 	public static final String RACE_ID = "RACE_ID";
 	public static final String PARK_NAME = "PARK_NAME";
 	public static final String RACE_DATE = "RACE_DATE";
+	public static final String ABSPOSITION = "ABSPOSITION";
+	public static final String POSITIONINGENDERGROUP = "POSITIONINGENDERGROUP";
+	public static final String CLUBID = "CLUBID";
+	public static final String CLUBNAME = "CLUBNAME";
+	public static final String CLUBLOGO = "CLUBLOGO";
 	
 	private static final String RACE_RESULT_TO_REQUEST = "SELECT RS.POSITION AS "+POSITION
 			+ ", CONCAT(RN.FIRSTNAME,' ',RN.LASTNAME) AS "+NAME+
@@ -27,6 +32,14 @@ public class TORequests {
 			+ ", CL.NAME AS "+CLUB+", RS.TIME AS "+TIME
 			+ " FROM RESULTS RS LEFT JOIN RUNNERS RN ON RS.RUNNER = RN.ID"
 			+ " LEFT JOIN CLUBS CL ON RS.CLUB=CL.ID WHERE RS.RACE = ";
+	
+	private static final String RUNNER_RESULT_INFO_REQUEST = "SELECT RS.POSITION AS " + ABSPOSITION
+			+ ", (SELECT COUNT(*)+1 FROM RESULTS WHERE RACE=RS.RACE AND POSITION<RS.POSITION AND GENDER = RS.GENDER) AS "
+			+ POSITIONINGENDERGROUP + ",RS.CLUB AS " + CLUBID + ",CL.NAME AS " + CLUBNAME + ", CL.SMALLLOGO AS "
+			+ CLUBLOGO + ", RS.RACE AS " + RACE_ID + ", RC.DATE AS " + RACE_DATE + ", PK.NAME AS " + PARK_NAME
+			+ ", RS.TIME AS " + TIME
+			+ " FROM RESULTS RS INNER JOIN RACES RC ON RS.RACE=RC.ID INNER JOIN PARKS PK ON RC.PARK=PK.ID "
+			+ "INNER JOIN CLUBS CL ON RS.CLUB = CL.ID WHERE RS.RUNNER =";
 	
 	private static final String RACELIST_REQUEST = "select rc.id as "+RACE_ID
 			+", pk.name as "+PARK_NAME+", rc.date as "+RACE_DATE+" from races rc inner"
@@ -42,5 +55,18 @@ public class TORequests {
 	 */
 	public static String fullRaceListRequest(){
 		return RACELIST_REQUEST;
+	}
+	
+	/**
+	 * 
+	 * @param runnerId
+	 * @return String sql-request
+	 */
+	public static String runnerResultInfoListRequest(long runnerId){
+		return RUNNER_RESULT_INFO_REQUEST+runnerId;
+	};
+	
+	public static void main(String...strings){
+		System.out.println(runnerResultInfoListRequest(80));
 	}
 }
