69c24b4908f7f8c2ef8ca7c51e3e602e8e047af7
misterlucky <misterlucky@tut.by>
2020-02-19 21:35:59 +0300

    dev: amend RaceClubResultTO

--- src/main/java/by/irun/domain/to/RaceClubResultTO.java
+++ src/main/java/by/irun/domain/to/RaceClubResultTO.java
@@ -20,109 +20,109 @@ public class RaceClubResultTO {
 	/**
 	 * @return the raceId
 	 */
-	protected long getRaceId() {
+	public long getRaceId() {
 		return raceId;
 	}
 	/**
 	 * @param raceId the raceId to set
 	 */
-	protected void setRaceId(long raceId) {
+	public void setRaceId(long raceId) {
 		this.raceId = raceId;
 	}
 	/**
 	 * @return the date
 	 */
-	protected Date getDate() {
+	public Date getDate() {
 		return date;
 	}
 	/**
 	 * @param date the date to set
 	 */
-	protected void setDate(Date date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 	/**
 	 * @return the parkName
 	 */
-	protected String getParkName() {
+	public String getParkName() {
 		return parkName;
 	}
 	/**
 	 * @param parkName the parkName to set
 	 */
-	protected void setParkName(String parkName) {
+	public void setParkName(String parkName) {
 		this.parkName = parkName;
 	}
 	/**
 	 * @return the runnerFirstName
 	 */
-	protected String getRunnerFirstName() {
+	public String getRunnerFirstName() {
 		return runnerFirstName;
 	}
 	/**
 	 * @param runnerFirstName the runnerFirstName to set
 	 */
-	protected void setRunnerFirstName(String runnerFirstName) {
+	public void setRunnerFirstName(String runnerFirstName) {
 		this.runnerFirstName = runnerFirstName;
 	}
 	/**
 	 * @return the runnerLastName
 	 */
-	protected String getRunnerLastName() {
+	public String getRunnerLastName() {
 		return runnerLastName;
 	}
 	/**
 	 * @param runnerLastName the runnerLastName to set
 	 */
-	protected void setRunnerLastName(String runnerLastName) {
+	public void setRunnerLastName(String runnerLastName) {
 		this.runnerLastName = runnerLastName;
 	}
 	/**
 	 * @return the runnerId
 	 */
-	protected long getRunnerId() {
+	public long getRunnerId() {
 		return runnerId;
 	}
 	/**
 	 * @param runnerId the runnerId to set
 	 */
-	protected void setRunnerId(long runnerId) {
+	public void setRunnerId(long runnerId) {
 		this.runnerId = runnerId;
 	}
 	/**
 	 * @return the timeInSeconds
 	 */
-	protected int getTimeInSeconds() {
+	public int getTimeInSeconds() {
 		return timeInSeconds;
 	}
 	/**
 	 * @param timeInSeconds the timeInSeconds to set
 	 */
-	protected void setTimeInSeconds(int timeInSeconds) {
+	public void setTimeInSeconds(int timeInSeconds) {
 		this.timeInSeconds = timeInSeconds;
 	}
 	/**
 	 * @return the absPosition
 	 */
-	protected int getAbsPosition() {
+	public int getAbsPosition() {
 		return absPosition;
 	}
 	/**
 	 * @param absPosition the absPosition to set
 	 */
-	protected void setAbsPosition(int absPosition) {
+	public void setAbsPosition(int absPosition) {
 		this.absPosition = absPosition;
 	}
 	/**
 	 * @return the positionInGenderGroup
 	 */
-	protected int getPositionInGenderGroup() {
+	public int getPositionInGenderGroup() {
 		return positionInGenderGroup;
 	}
 	/**
 	 * @param positionInGenderGroup the positionInGenderGroup to set
 	 */
-	protected void setPositionInGenderGroup(int positionInGenderGroup) {
+	public void setPositionInGenderGroup(int positionInGenderGroup) {
 		this.positionInGenderGroup = positionInGenderGroup;
 	}
 
