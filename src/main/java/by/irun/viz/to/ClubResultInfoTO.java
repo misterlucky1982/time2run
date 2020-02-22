5cc360a4c206c5b88a851e5cafbc2a9db00c075d
misterlucky <misterlucky@tut.by>
2020-02-22 16:52:29 +0300

    dev: ClubResultInfoTO

---
+++ src/main/java/by/irun/viz/to/ClubResultInfoTO.java
@@ -0,0 +1,93 @@
+package by.irun.viz.to;
+
+/**
+ * Provides information about race result
+ * <p>
+ *  in the list of  another club`s results 
+ *  <p>
+ *  for front-end representation
+ * @author A.Dubovik
+ */
+public class ClubResultInfoTO {
+
+	private String position;
+	private String raceName;
+	private String runnerName;
+	private String time;
+	private String linkToRace;
+	private String linkToRunner;
+	/**
+	 * @return the position
+	 */
+	public String getPosition() {
+		return position;
+	}
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	/**
+	 * @return the raceName
+	 */
+	public String getRaceName() {
+		return raceName;
+	}
+	/**
+	 * @param raceName the raceName to set
+	 */
+	public void setRaceName(String raceName) {
+		this.raceName = raceName;
+	}
+	/**
+	 * @return the runnerName
+	 */
+	public String getRunnerName() {
+		return runnerName;
+	}
+	/**
+	 * @param runnerName the runnerName to set
+	 */
+	public void setRunnerName(String runnerName) {
+		this.runnerName = runnerName;
+	}
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+	/**
+	 * @return the linkToRace
+	 */
+	public String getLinkToRace() {
+		return linkToRace;
+	}
+	/**
+	 * @param linkToRace the linkToRace to set
+	 */
+	public void setLinkToRace(String linkToRace) {
+		this.linkToRace = linkToRace;
+	}
+	/**
+	 * @return the linkToRunner
+	 */
+	public String getLinkToRunner() {
+		return linkToRunner;
+	}
+	/**
+	 * @param linkToRunner the linkToRunner to set
+	 */
+	public void setLinkToRunner(String linkToRunner) {
+		this.linkToRunner = linkToRunner;
+	}
+	
+	
+}
