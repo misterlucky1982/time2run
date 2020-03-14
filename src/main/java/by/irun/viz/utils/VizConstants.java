ab834a2d2a8bc4b91004d9df9fafdab0a1e8e3cf
misterlucky <misterlucky@tut.by>
2020-03-14 20:06:54 +0300

    dev: small logos for race result page

--- src/main/java/by/irun/viz/utils/VizConstants.java
+++ src/main/java/by/irun/viz/utils/VizConstants.java
@@ -7,14 +7,16 @@ package by.irun.viz.utils;
  */
 public class VizConstants {
 	
-	public static final String NO_FOTO_AVATAR_MAN_BY = "../../images/nofoto_man_by.png";
-	public static final String NO_FOTO_AVATAR_MAN_RU = "../../images/nofoto_man_ru.png";
-	public static final String NO_FOTO_AVATAR_MAN_EN = "../../images/nofoto_man_en.png";
-	public static final String NO_FOTO_AVATAR_WOMAN_BY = "../../images/nofoto_woman_by.png";
-	public static final String NO_FOTO_AVATAR_WOMAN_RU = "../../images/nofoto_woman_ru.png";
-	public static final String NO_FOTO_AVATAR_WOMAN_EN = "../../images/nofoto_woman_en.png";
+	public static final String NO_FOTO_AVATAR_MAN_BY = "../../images/runners/nofoto_man_by.png";
+	public static final String NO_FOTO_AVATAR_MAN_RU = "../../images/runners/nofoto_man_ru.png";
+	public static final String NO_FOTO_AVATAR_MAN_EN = "../../images/runners/nofoto_man_en.png";
+	public static final String NO_FOTO_AVATAR_WOMAN_BY = "../../images/runners/nofoto_woman_by.png";
+	public static final String NO_FOTO_AVATAR_WOMAN_RU = "../../images/runners/nofoto_woman_ru.png";
+	public static final String NO_FOTO_AVATAR_WOMAN_EN = "../../images/runners/nofoto_woman_en.png";
 	public static final String CLUBLOGO_IS_NOT_UPLOADED_BY = "../../images/clubs/club_nologo_by.png";
 	public static final String CLUBLOGO_IS_NOT_UPLOADED_RU = "../../images/clubs/club_nologo_ru.png";
 	public static final String CLUBLOGO_IS_NOT_UPLOADED_EN = "../../images/clubs/club_nologo_en.png";
+	public static final String RACE_RESULT_NO_AVATAR_MALE = "../../images/runners/no_avatar_male.png";
+	public static final String RACE_RESULT_NO_AVATAR_FEMALE = "../../images/runners/no_avatar_female.png";
 
 }
