6f327483548947e440dd3a17b7fc601363a10e27
misterlucky <misterlucky@tut.by>
2020-04-21 19:55:12 +0300

    test: apply test data on remote

--- src/main/java/by/irun/config/ApplicationConstants.java
+++ src/main/java/by/irun/config/ApplicationConstants.java
@@ -9,9 +9,15 @@ public class ApplicationConstants {
 	
 	private ApplicationConstants(){}
 	
+	/*
 	public static final String DB_URL = "jdbc:postgresql://localhost/time2run";
 	public static final String DB_USERNAME = "postgres";
 	public static final String DB_PASSWORD = "gfhjkm";
 	public static final String URL = "http://localhost:8080";
+	*/
+	public static final String DB_URL = "jdbc:postgresql://10.100.2.55:5432/dsp";
+	public static final String DB_USERNAME = "webadmin";
+	public static final String DB_PASSWORD = "MYZkds77488";
+	public static final String URL = "http://env-2982050.mycloud.by/";
 
 }
