9547791e008c276372ba7202d1027347c2cf21af
misterlucky1982 <misterlucky@tut.by>
2020-05-15 18:26:39 +0300

    dev: amend application config

--- src/main/java/by/irun/config/ApplicationConfig.java
+++ src/main/java/by/irun/config/ApplicationConfig.java
@@ -3,9 +3,12 @@ package by.irun.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import by.irun.controller.ControllerConstants;
 import by.irun.locale.AppLocales;
 
 @Configuration
@@ -18,5 +21,16 @@ public class ApplicationConfig implements WebMvcConfigurer{
 	    return slr;
 	}
 
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName(ControllerConstants.LOCALE_PARAM);
+	    return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
 
 }
--- src/main/java/by/irun/controller/ControllerConstants.java
+++ src/main/java/by/irun/controller/ControllerConstants.java
@@ -19,4 +19,6 @@ public class ControllerConstants {
 	public static final String FROM_DATE_WARNING_REQEST_PARAMVALUE = "from";
 	public static final String FOR_DATE_WARNING_REQEST_PARAMVALUE = "for";
 	public static final String BOTH_FROM_FOR_DATE_WARNING_REQEST_PARAMVALUE = "both";
+	
+	public static final String LOCALE_PARAM = "locale";
 }
